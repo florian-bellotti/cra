bec7e0107ff8ff0b342fb704166a5f04b515d503
bellotti florian <florian.bellotti@viacesi.fr>
2017-07-11 21:13:20 +0200

    :bug: fix unit test

--- user-service/src/test/java/com/fbellotti/user/ApiTest.java
+++ user-service/src/test/java/com/fbellotti/user/ApiTest.java
@@ -16,6 +16,7 @@ import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.ext.web.codec.BodyCodec;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,14 +54,14 @@ public class ApiTest {
 
     JsonObject user = new JsonObject()
       .put("_id", "111")
-      .put("username", "florian");
+      .put("username", "florian")
+      .put("password", "florian");
 
     Future<JsonObject> postRequest = Future.future();
     webClient.post("/users")
       .as(BodyCodec.jsonObject())
       .sendJsonObject(user, ar -> {
         if (ar.succeeded()) {
-          System.out.println("w");
           HttpResponse<JsonObject> postResponse = ar.result();
           postRequest.complete(postResponse.body());
         } else {
@@ -68,13 +69,31 @@ public class ApiTest {
         }
       });
 
-    Future<JsonObject> getRequest = Future.future();
+    Future<JsonArray> getAllRequest = Future.future();
     postRequest.compose(h -> {
+      webClient.get("/users")
+        .as(BodyCodec.jsonArray())
+        .send(ar -> {
+          if (ar.succeeded()) {
+            HttpResponse<JsonArray> getAllResponse = ar.result();
+            Assert.assertEquals("[{\"_id\":\"111\",\"username\":\"florian\",\"password\":\"florian\"}]",
+              getAllResponse.body().encode());
+            getAllRequest.complete(getAllResponse.body());
+          } else {
+            context.fail(ar.cause());
+          }
+        });
+    }, getAllRequest);
+
+    Future<JsonObject> getRequest = Future.future();
+    getAllRequest.compose(h -> {
       webClient.get("/users/111")
         .as(BodyCodec.jsonObject())
         .send(ar -> {
           if (ar.succeeded()) {
             HttpResponse<JsonObject> getResponse = ar.result();
+            Assert.assertEquals("{\"_id\":\"111\",\"username\":\"florian\",\"password\":\"florian\"}",
+              getResponse.body().encode());
             getRequest.complete(getResponse.body());
           } else {
             context.fail(ar.cause());
@@ -88,8 +107,8 @@ public class ApiTest {
         .as(BodyCodec.jsonObject())
         .send(ar -> {
           if (ar.succeeded()) {
-            HttpResponse<JsonObject> getResponse = ar.result();
-            deleteRequest.complete(getResponse.body());
+            HttpResponse<JsonObject> deleteResponse = ar.result();
+            deleteRequest.complete(deleteResponse.body());
           } else {
             context.fail(ar.cause());
           }
